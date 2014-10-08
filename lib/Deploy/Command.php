b7ed9adb9fd5a093439f821ff4c1aa3d4e33a239
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-09 01:39:14 +0300

    Command class is much smarter now

--- lib/Deploy/Command.php
+++ lib/Deploy/Command.php
@@ -7,9 +7,15 @@ use \Qobo\Pattern\Pattern;
  * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
  */
 class Command {
+
+	const STATUS_SUCCESS = 0;
  
 	private $type;
 	private $command;
+
+	private $lastCommand;
+	private $lastStatus;
+	private $lastOutput;
 	
 	/**
 	 * Constructor
@@ -40,25 +46,50 @@ class Command {
 	public function getCommand() {
 		return $this->command;
 	}
+
+	/**
+	 * Get a fully formatted command line for previous run
+	 * 
+	 * @return string
+	 */
+	public function getLastCommand() {
+		return $this->lastCommand;
+	}
 	
+	/**
+	 * Get status from the last run
+	 * 
+	 * @return integer
+	 */
+	public function getLastStatus() {
+		return $this->lastStatus;
+	}
+
+	/**
+	 * Get output from the last run
+	 * 
+	 * @return array
+	 */
+	public function getLastOutput() {
+		return $this->lastOutput;
+	}
+
 	/**
 	 * Run command
 	 * 
+	 * @throws RuntimeException
 	 * @param array $params Associative array of key-values to replace placedholders
 	 * @return void
 	 */
 	public function run(array $params = array()) {
-		$command = $this->command->parse($params);
-		print "Executing: $command ... ";
-		$out = array('');
-		$result = exec($command, $out, $status);
-		if ($status > 0) {
-			print "FAIL\n";
-			print $result . "\n";
-		}
-		else {
-			print "OK\n";
-			print $result . "\n";
+		$this->lastCommand = $this->command->parse($params);
+	   	$this->lastCommand .= ' 2>/dev/null'; // supress stderr output
+		
+		$this->lastOutput = array(''); // reset output
+		
+		$lastLine = exec($this->lastCommand, $this->lastOutput, $this->lastStatus);
+		if ($this->lastStatus <> self::STATUS_SUCCESS) {
+			throw new \RuntimeException("Non-zero exist status [$this->lastStatus] when executing command [$this->lastCommand]");
 		}
 	}
 
