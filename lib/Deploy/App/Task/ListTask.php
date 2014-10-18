f1460c0d5c61200244e3305d645245a3c43293da
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-18 19:21:01 +0300

    Simple output for project list with -s or --simple (closes #9)

--- lib/Deploy/App/Task/ListTask.php
+++ lib/Deploy/App/Task/ListTask.php
@@ -3,6 +3,8 @@ namespace Deploy\App\Task;
 
 use \Deploy\Config\Factory;
 
+use \GetOptionKit\OptionCollection;
+
 class ListTask extends BaseTask{
 
 	protected static $description = 'List available projects';
@@ -14,11 +16,33 @@ class ListTask extends BaseTask{
 	public function run() {
 		$projects = Factory::getList();
 		
-		print "\n";
-		print "Available projects:\n\n";
-		foreach ($projects as $project) {
-			print "\t- " . $project . "\n";
+		if (!empty($this->params['simple']) && $this->params['simple']) {
+			foreach ($projects as $project) {
+				print "$project\n";
+			}
+		}
+		else {
+			print "\n";
+			print "Available projects:\n\n";
+			foreach ($projects as $project) {
+				print "\t- " . $project . "\n";
+			}
 		}
 	}
 	
+	/***
+	 * Get command line options spec
+	 * 
+	 * @return OptionCollection
+	 */
+	public static function getParams() {
+		$result = new OptionCollection;
+		
+		$result->add('s|simple', 'simple output (one per line)')
+			->isa('Boolean');
+
+		return $result;
+	}
+
+
 }
