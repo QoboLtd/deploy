dbc32b6a4af75d9e4c62fe692829d0a8c2acb74e
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-27 14:48:46 +0200

    More doc blocks

--- lib/Deploy/Exception/ParseException.php
+++ lib/Deploy/Exception/ParseException.php
@@ -1,10 +1,21 @@
 <?php
 namespace Deploy\Exception;
-
+/**
+ * ParseException
+ * 
+ * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
+ */
 class ParseException extends \InvalidArgumentException {
-
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param string $message Parsing error message
+	 * @param integer $code Code
+	 * @param Exception $previous Previous exception
+	 * @return object
+	 */
 	public function __construct($message, $code = 0, Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
-	
 }
